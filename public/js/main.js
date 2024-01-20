cc3fd3ed6cc5825b573ee71e2bdb839c0e42ef84
ArtusAbelard <tribelha@gmail.com>
2024-01-20 22:09:25 +0100

    final

---
+++ public/js/main.js
@@ -0,0 +1,247 @@
+let btnbottle = document.querySelectorAll(".minibottle")
+let divinbottle = document.querySelectorAll(".divinbottle")
+let divwhite = document.querySelectorAll(".divwhite")
+let txtminibottle = document.querySelectorAll(".txtminibottle")
+
+//! je sais je me suis fais chier pour rien mais je ne savais pas comment faire une boucle de tout sa ...
+
+btnbottle[0].addEventListener("click",()=>{
+
+    divwhite[7].style.display="flex"
+    divwhite[6].style.display="none"
+    divwhite[5].style.display="none"
+    divwhite[4].style.display="none"
+    divwhite[3].style.display="none"
+    divwhite[2].style.display="none"
+    divwhite[1].style.display="none"
+    divwhite[0].style.display="none"
+
+    btnbottle[0].style.background="#5FA8F6"
+    btnbottle[1].style.background="white"
+    btnbottle[2].style.background="white"
+    btnbottle[3].style.background="white"
+    btnbottle[4].style.background="white"
+    btnbottle[5].style.background="white"
+    btnbottle[6].style.background="white"
+    btnbottle[7].style.background="white"
+
+    txtminibottle[0].style.color="white"
+    txtminibottle[1].style.color="#284389"
+    txtminibottle[2].style.color="#284389"
+    txtminibottle[3].style.color="#284389"
+    txtminibottle[4].style.color="#284389"
+    txtminibottle[5].style.color="#284389"
+    txtminibottle[6].style.color="#284389"
+    txtminibottle[7].style.color="#284389"
+   
+
+})
+btnbottle[1].addEventListener("click",()=>{
+
+    divwhite[7].style.display="none"
+    divwhite[6].style.display="flex"
+    divwhite[5].style.display="none"
+    divwhite[4].style.display="none"
+    divwhite[3].style.display="none"
+    divwhite[2].style.display="none"
+    divwhite[1].style.display="none"
+    divwhite[0].style.display="none"
+
+    btnbottle[0].style.background="#5FA8F6"
+    btnbottle[1].style.background="#5FA8F6"
+    btnbottle[2].style.background="white"
+    btnbottle[3].style.background="white"
+    btnbottle[4].style.background="white"
+    btnbottle[5].style.background="white"
+    btnbottle[6].style.background="white"
+    btnbottle[7].style.background="white"
+
+    txtminibottle[0].style.color="white"
+    txtminibottle[1].style.color="white"
+    txtminibottle[2].style.color="#284389"
+    txtminibottle[3].style.color="#284389"
+    txtminibottle[4].style.color="#284389"
+    txtminibottle[5].style.color="#284389"
+    txtminibottle[6].style.color="#284389"
+    txtminibottle[7].style.color="#284389"
+    
+})
+btnbottle[2].addEventListener("click",()=>{
+
+    divwhite[7].style.display="none"
+    divwhite[6].style.display="none"
+    divwhite[5].style.display="flex"
+    divwhite[4].style.display="none"
+    divwhite[3].style.display="none"
+    divwhite[2].style.display="none"
+    divwhite[1].style.display="none"
+    divwhite[0].style.display="none"
+
+    btnbottle[0].style.background="#5FA8F6"
+    btnbottle[1].style.background="#5FA8F6"
+    btnbottle[2].style.background="#5FA8F6"
+    btnbottle[3].style.background="white"
+    btnbottle[4].style.background="white"
+    btnbottle[5].style.background="white"
+    btnbottle[6].style.background="white"
+    btnbottle[7].style.background="white"
+
+    txtminibottle[0].style.color="white"
+    txtminibottle[1].style.color="white"
+    txtminibottle[2].style.color="white"
+    txtminibottle[3].style.color="#284389"
+    txtminibottle[4].style.color="#284389"
+    txtminibottle[5].style.color="#284389"
+    txtminibottle[6].style.color="#284389"
+    txtminibottle[7].style.color="#284389"
+})
+btnbottle[3].addEventListener("click",()=>{
+
+    divwhite[7].style.display="none"
+    divwhite[6].style.display="none"
+    divwhite[5].style.display="none"
+    divwhite[4].style.display="flex"
+    divwhite[3].style.display="none"
+    divwhite[2].style.display="none"
+    divwhite[1].style.display="none"
+    divwhite[0].style.display="none"
+
+
+    btnbottle[0].style.background="#5FA8F6"
+    btnbottle[1].style.background="#5FA8F6"
+    btnbottle[2].style.background="#5FA8F6"
+    btnbottle[3].style.background="#5FA8F6"
+    btnbottle[4].style.background="white"
+    btnbottle[5].style.background="white"
+    btnbottle[6].style.background="white"
+    btnbottle[7].style.background="white"
+
+    txtminibottle[0].style.color="white"
+    txtminibottle[1].style.color="white"
+    txtminibottle[2].style.color="white"
+    txtminibottle[3].style.color="white"
+    txtminibottle[4].style.color="#284389"
+    txtminibottle[5].style.color="#284389"
+    txtminibottle[6].style.color="#284389"
+    txtminibottle[7].style.color="#284389"
+})
+btnbottle[4].addEventListener("click",()=>{
+
+    divwhite[7].style.display="none"
+    divwhite[6].style.display="none"
+    divwhite[5].style.display="none"
+    divwhite[4].style.display="none"
+    divwhite[3].style.display="flex"
+    divwhite[2].style.display="none"
+    divwhite[1].style.display="none"
+    divwhite[0].style.display="none"
+
+    btnbottle[0].style.background="#5FA8F6"
+    btnbottle[1].style.background="#5FA8F6"
+    btnbottle[2].style.background="#5FA8F6"
+    btnbottle[3].style.background="#5FA8F6"
+    btnbottle[4].style.background="#5FA8F6"
+    btnbottle[5].style.background="white"
+    btnbottle[6].style.background="white"
+    btnbottle[7].style.background="white"
+
+    txtminibottle[0].style.color="white"
+    txtminibottle[1].style.color="white"
+    txtminibottle[2].style.color="white"
+    txtminibottle[3].style.color="white"
+    txtminibottle[4].style.color="white"
+    txtminibottle[5].style.color="#284389"
+    txtminibottle[6].style.color="#284389"
+    txtminibottle[7].style.color="#284389"
+})
+btnbottle[5].addEventListener("click",()=>{
+
+    divwhite[7].style.display="none"
+    divwhite[6].style.display="none"
+    divwhite[5].style.display="none"
+    divwhite[4].style.display="none"
+    divwhite[3].style.display="none"
+    divwhite[2].style.display="flex"
+    divwhite[1].style.display="none"
+    divwhite[0].style.display="none"
+
+
+    btnbottle[0].style.background="#5FA8F6"
+    btnbottle[1].style.background="#5FA8F6"
+    btnbottle[2].style.background="#5FA8F6"
+    btnbottle[3].style.background="#5FA8F6"
+    btnbottle[4].style.background="#5FA8F6"
+    btnbottle[5].style.background="#5FA8F6"
+    btnbottle[6].style.background="white"
+    btnbottle[7].style.background="white"
+
+    txtminibottle[0].style.color="white"
+    txtminibottle[1].style.color="white"
+    txtminibottle[2].style.color="white"
+    txtminibottle[3].style.color="white"
+    txtminibottle[4].style.color="white"
+    txtminibottle[5].style.color="white"
+    txtminibottle[6].style.color="#284389"
+    txtminibottle[7].style.color="#284389"
+})
+btnbottle[6].addEventListener("click",()=>{
+
+    divwhite[7].style.display="none"
+    divwhite[6].style.display="none"
+    divwhite[5].style.display="none"
+    divwhite[4].style.display="none"
+    divwhite[3].style.display="none"
+    divwhite[2].style.display="none"
+    divwhite[1].style.display="flex"
+    divwhite[0].style.display="none"
+
+
+    btnbottle[0].style.background="#5FA8F6"
+    btnbottle[1].style.background="#5FA8F6"
+    btnbottle[2].style.background="#5FA8F6"
+    btnbottle[3].style.background="#5FA8F6"
+    btnbottle[4].style.background="#5FA8F6"
+    btnbottle[5].style.background="#5FA8F6"
+    btnbottle[6].style.background="#5FA8F6"
+    btnbottle[7].style.background="white"
+
+    txtminibottle[0].style.color="white"
+    txtminibottle[1].style.color="white"
+    txtminibottle[2].style.color="white"
+    txtminibottle[3].style.color="white"
+    txtminibottle[4].style.color="white"
+    txtminibottle[5].style.color="white"
+    txtminibottle[6].style.color="white"
+    txtminibottle[7].style.color="#284389"
+    
+})
+btnbottle[7].addEventListener("click",()=>{
+
+    divwhite[7].style.display="none"
+    divwhite[6].style.display="none"
+    divwhite[5].style.display="none"
+    divwhite[4].style.display="none"
+    divwhite[3].style.display="none"
+    divwhite[2].style.display="none"
+    divwhite[1].style.display="none"
+    divwhite[0].style.display="flex"
+
+
+    btnbottle[0].style.background="#5FA8F6"
+    btnbottle[1].style.background="#5FA8F6"
+    btnbottle[2].style.background="#5FA8F6"
+    btnbottle[3].style.background="#5FA8F6"
+    btnbottle[4].style.background="#5FA8F6"
+    btnbottle[5].style.background="#5FA8F6"
+    btnbottle[6].style.background="#5FA8F6"
+    btnbottle[7].style.background="#5FA8F6"
+
+    txtminibottle[0].style.color="white"
+    txtminibottle[1].style.color="white"
+    txtminibottle[2].style.color="white"
+    txtminibottle[3].style.color="white"
+    txtminibottle[4].style.color="white"
+    txtminibottle[5].style.color="white"
+    txtminibottle[6].style.color="white"
+    txtminibottle[7].style.color="white"
+})
